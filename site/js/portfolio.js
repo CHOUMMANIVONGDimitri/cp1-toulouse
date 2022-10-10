85f68dedd12e0305e2bc9f646e45e8da73530b8a
CHOUMMANIVONGDimitri <dimitrichoummanivong@gmail.com>
2022-10-10 13:53:50 +0200

    portfolio Done(without Bonus exercice)

--- site/js/portfolio.js
+++ site/js/portfolio.js
@@ -3,13 +3,16 @@ const avatar = document.getElementById("avatarImg");
 avatar.addEventListener("click", () => {
   avatar.src = "./image/logo-wcs.png";
 });
-// 2nd task & 3rd task
+// 2nd task & 3rd task & 4th task
 
 const spanTitle = document.getElementById("name");
 const colorBg = document.querySelector(".pink-bg");
 const buttonModifText = document.getElementById("btn-changeText");
+const pColor = document.querySelector(".pink-text");
 buttonModifText.addEventListener("click", () => {
-  colorBg.style.backgroundColor = `${prompt("entre une couleur")}`;
+  let colorValue = `${prompt("entre une couleur")}`;
+  colorBg.style.backgroundColor = colorValue;
+  pColor.style.color = colorValue;
   spanTitle.innerText = prompt("change les valeur");
   spanTitle.style.color = "white";
 });
